1874f00d2385c2bb56e48945b9990dcecc71dcf4
Giovanni Finetto <finettogs@gmail.com>
2025-05-16 15:04:17 -0300

    Ajustes para melhorar estabilidade dos testes no CI/CD

--- tests/cart/test_cart.py
+++ tests/cart/test_cart.py
@@ -36,18 +36,23 @@ def test_correct_item(setup):
     cart_product_name = page.locator(".inventory_item_name").first.text_content()
     assert product_name == cart_product_name
 
+# No seu arquivo test_cart.py, modifique o teste test_add_multiple_items_to_cart
 def test_add_multiple_items_to_cart(setup):
     page = setup
     login_page = LoginPage(page)
     inventory_page = InventoryPage(page)
     login_page.goto()
+    page.wait_for_selector("#login-button", state="visible")
     login_page.login("standard_user", "secret_sauce")
+    page.wait_for_selector(".inventory_container", state="visible")
     inventory_page.add_item_to_cart(0)
     inventory_page.add_item_to_cart(1)
     inventory_page.add_item_to_cart(2)
+    page.wait_for_selector(".shopping_cart_badge", state="visible")
     cart_count = page.text_content(".shopping_cart_badge")
     assert cart_count == "3"
     inventory_page.go_to_cart()
+    page.wait_for_selector(".cart_list", state="visible")
     assert "cart.html" in page.url
     assert page.locator(".cart_item").count() == 3
 
